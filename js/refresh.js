111da1beef9e577e10060d733651b39c936acc2b
ひよ/hiyo <32918187+hiyo-ac@users.noreply.github.com>
2017-10-21 18:17:01 +0900

    Update refresh.js
    delete comment
    change interval

--- js/refresh.js
+++ js/refresh.js
@@ -11,34 +11,16 @@ function getCurrentTabUrl(callback) {
     callback(url);
   });
 }
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
+
 function getSavedRefreshInterval(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
   chrome.storage.sync.get(url, (items) => {
     callback(chrome.runtime.lastError ? null : items[url]);
   });
 }
 
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
 function saveRefreshInterval(url, interval) {
   var items = {};
   items[url] = interval;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
   chrome.storage.sync.set(items);
 }
 
@@ -66,5 +48,5 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function reloadTab(interval){
-  setInterval(function(){chrome.tabs.reload();}, Number(interval)*1000*5);
+  setInterval(function(){chrome.tabs.reload();}, Number(interval)*1000*60);
 }
